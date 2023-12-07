8bff155e58d0ddc9980e5bdcfe1e91fd1939bc8e
doroshkent <108677278+doroshkent@users.noreply.github.com>
2023-12-07 17:25:05 +0100

    add Header layout

---
+++ src/layout/Header.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Logo} from "../components/logo/Logo";
+import {Menu} from "../components/menu/Menu";
+import styled from "styled-components";
+
+export const Header = () => {
+  return (
+    <StyledHeader>
+      <Logo />
+      <Menu />
+    </StyledHeader>
+  );
+};
+
+const StyledHeader = styled.header`
+  background-color: #87d76c;
+  display: flex;
+  justify-content: space-between;
+`
