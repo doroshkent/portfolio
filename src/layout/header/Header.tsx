dd306637aa6c6e1d134bc24ad67590f7c8036001
doroshkent <108677278+doroshkent@users.noreply.github.com>
2023-12-08 11:00:48 +0100

    refactor Menu component

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -3,11 +3,13 @@ import { Logo } from "components/logo/Logo";
 import { Menu } from "components/menu/Menu";
 import styled from "styled-components";
 
+const items = ['Home', 'Skills', 'Works', 'Testimony', 'Contact']
+
 export const Header = () => {
   return (
     <StyledHeader>
       <Logo/>
-      <Menu/>
+      <Menu menuItems={items}/>
     </StyledHeader>
   );
 };
