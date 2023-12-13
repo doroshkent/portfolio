a38f4d1798772ce4a1fc7da033ccaa816b951fec
doroshkent <108677278+doroshkent@users.noreply.github.com>
2023-12-13 18:19:55 +0100

    refactor footer

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -2,24 +2,30 @@ import React from 'react';
 import styled from "styled-components";
 import { Icon } from "components/icon/Icon";
 import { FlexWrapper } from "components/FlexWrapper";
+import { Text } from "components/Text"
 
-const iconsItems = ["instagram", "telegram", "vk", "linkedin"]
+const iconsItems = [ "gmail", "telegram", "linkedin" ]
 
 export const Footer = () => {
   return (
     <StyledFooter>
-      <FlexWrapper direction={"column"} align={"center"}>
-        <Name>Anastasiya</Name>
-        <SocialList>
-          {iconsItems.map((item, index) => {
-            return <SocialItem key={index}>
-              <SocialLink>
-                <Icon iconId={item} height={"21px"} width={"21px"} viewBox={"0 0 21px 21px"}/>
-              </SocialLink>
-            </SocialItem>
-          })}
-        </SocialList>
-        <Copyright>© 2023 Svetlana Dyablo, All Rights Reserved.</Copyright>
+      <FlexWrapper justify={"space-around"}>
+        <div>
+          <Text>My social media links:</Text>
+          <SocialList>
+            { iconsItems.map((item, index) => {
+              return <SocialItem key={ index }>
+                <SocialLink>
+                  <Icon iconId={ item } height={ "30px" } width={ "30px" } viewBox={ "0 0 30px 30px" }/>
+                </SocialLink>
+              </SocialItem>
+            }) }
+          </SocialList>
+        </div>
+        <div>
+          <Text>More projects I've worked on</Text>
+          <Text><SocialLink href={"#"}><Icon iconId={"github"}/>@doroshkent</SocialLink> on github</Text>
+        </div>
       </FlexWrapper>
     </StyledFooter>
   );
@@ -28,10 +34,6 @@ export const Footer = () => {
 const StyledFooter = styled.footer`
   background-color: #d1eaca;
   min-height: 20vh;
-`
-
-const Name = styled.span`
-
 `
 
 const SocialList = styled.ul`
@@ -45,7 +47,4 @@ const SocialItem = styled.li`
 
 const SocialLink = styled.a`
 
-`
-
-const Copyright = styled.small`
 `
