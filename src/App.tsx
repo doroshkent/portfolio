c7f124a5e30afdb56cea728747fd99505a51ba2d
doroshkent <108677278+doroshkent@users.noreply.github.com>
2023-12-13 15:14:24 +0100

    refactor main section and common structure

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,7 @@ import { Header } from "layout/header/Header";
 import { Main } from "layout/sections/main/Main";
 import { Skills } from "layout/sections/skills/Skills";
 import { Works } from "layout/sections/works/Works";
-import { Testimony } from "layout/sections/testimony/Testimony";
 import { Contact } from "layout/sections/contact/Contact";
-import { Slogan } from "layout/sections/slogan/Slogan";
 import { Footer } from "layout/footer/Footer";
 
 
@@ -16,9 +14,7 @@ function App() {
       <Main/>
       <Skills/>
       <Works/>
-      <Testimony/>
       <Contact/>
-      <Slogan/>
       <Footer/>
     </div>
   );
--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -2,15 +2,18 @@ import React from 'react';
 import photo from 'assets/img/photo.webp'
 import styled from "styled-components";
 import { FlexWrapper } from 'components/FlexWrapper';
+import { Button } from "components/button/Button";
+import { StyledText } from "components/StyledText";
 
 export const Main = () => {
   return (
     <StyledMain>
       <FlexWrapper align="center" justify="space-around">
         <div>
-          <span>Hi There</span>
-          <Name>I am Anastasiya Darashkevich</Name>
-          <MainHeading>Front End Developer</MainHeading>
+          <span>Hi👋, I’m a </span>
+          <MainHeading>front-end developer</MainHeading>
+          <StyledText>I’m Anastasiya Darashkevich, a developer dedicated to making the world a better place one line of code at a time.</StyledText>
+          <Button>Hire me</Button>
         </div>
         <Photo src={photo} alt={"My photo"} />
       </FlexWrapper>
@@ -30,9 +33,5 @@ const StyledMain = styled.section`
 `
 
 const MainHeading = styled.h1`
-
-`
-
-const Name = styled.h2`
 
 `
