c5f68b55892b6b5fabcb191e0eff75d67b63cb4a
doroshkent <108677278+doroshkent@users.noreply.github.com>
2023-12-17 17:36:46 +0100

    add box-shadow

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -23,6 +23,8 @@ export const Header = () => {
 const StyledHeader = styled.header`
   background-color: #FCFCFC;
   padding: 12px 0;
+  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.20), 0 2px 1px 0 rgba(0, 0, 0, 0.12), 0 1px 1px 0 rgba(0, 0, 0, 0.14);
+  
   position: fixed;
   top: 0;
   left: 0; 
