0f06ce7287dee5141773999f3f838c0c4dfe065d
doroshkent <108677278+doroshkent@users.noreply.github.com>
2023-12-24 21:16:32 +0100

    change title's weight and size

--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -1,29 +1,20 @@
 import styled from "styled-components";
 import { theme } from "styles/Theme";
 import React from "react";
-import { Icon } from "components/icon/Icon";
 import { font } from "styles/Common";
 
 type SectionTitlePropsType = {
   isActive?: boolean
   isWhite?: boolean
   marginBottom?: string
-  children?: React.ReactNode
 }
 
-export const SectionTitle = (props: SectionTitlePropsType) => {
-  return <StyledSectionTitle isWhite={ props.isWhite } isActive={ props.isActive } marginBottom={ props.marginBottom }>
-    <Icon iconId={ "gears" } width="25" height="23" viewBox="0 0 25 23"/>
-    <span>{ props.children }</span>
-  </StyledSectionTitle>
-}
-
-const StyledSectionTitle = styled.h2<SectionTitlePropsType>`
+export const SectionTitle = styled.h2<SectionTitlePropsType>`
   text-align: center;
   margin-bottom: ${ props => props.marginBottom || "60px" };
   ${ props => font({
-    weight: 600,
-    Fmax: 30,
+    weight: 500,
+    Fmax: 40,
     Fmin: 25,
     color: props.isWhite ? theme.colors.tertiaryFont : theme.colors.accent
   }) };
