88f5866848c4c207e7bb8df13328de06b8d53a4d
doroshkent <108677278+doroshkent@users.noreply.github.com>
2023-12-25 15:13:48 +0100

    refactor header

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,37 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Logo } from "components/logo/Logo";
-import styled from "styled-components";
+import { S } from "./Header_Styles";
 import { Container } from "components/Container";
 import { FlexWrapper } from 'components/FlexWrapper';
-import { HeaderMenu } from "layout/header/header-menu/HeaderMenu";
-import { MobileHeaderMenu } from "layout/header/mobile-header-menu/MobileHeaderMenu";
+import { DesktopMenu } from "layout/header/headerMenu/desktopMenu/DesktopMenu";
+import { MobileMenu } from "layout/header/headerMenu/mobileMenu/MobileMenu";
 
-const items = ['Skills', 'Projects', 'Contact me']
+const items = [ 'Skills', 'Projects', 'Contact me' ];
 
-export const Header = () => {
+export const Header: React.FC = () => {
+  const [ width, setWidth ] = useState(window.innerWidth);
+  const breakpoint = 576;
+
+  useEffect(() => {
+    const handleWindowResize = () => setWidth(window.innerWidth)
+    window.addEventListener("resize", handleWindowResize);
+
+    return () => window.removeEventListener("resize", handleWindowResize);
+  }, []);
+
   return (
-    <StyledHeader>
+    <S.Header>
       <Container>
-        <FlexWrapper justify={"space-between"} align={"center"}>
+        <FlexWrapper justify="space-between" align="center">
           <Logo/>
-          <HeaderMenu menuItems={items}/>
-          <MobileHeaderMenu menuItems={items}/>
+          { width < breakpoint ? <MobileMenu menuItems={ items }/> : <DesktopMenu menuItems={ items }/> }
         </FlexWrapper>
       </Container>
-    </StyledHeader>
+    </S.Header>
   );
 };
-
-const StyledHeader = styled.header`
-  min-height: 72px;
-  background-color: #FCFCFC;
-  padding: 12px 0;
-  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.20), 0 2px 1px 0 rgba(0, 0, 0, 0.12), 0 1px 1px 0 rgba(0, 0, 0, 0.14);
-  //overflow: hidden;
-
-  position: fixed;
-  top: 0;
-  left: 0; 
-  right: 0;
-  z-index: 9999;
-`
---
+++ src/layout/header/Header_Styles.ts
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+const Header = styled.header`
+  min-height: 72px;
+  background-color: #FCFCFC;
+  padding: 12px 0;
+  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.20), 0 2px 1px 0 rgba(0, 0, 0, 0.12), 0 1px 1px 0 rgba(0, 0, 0, 0.14);
+
+  position: fixed;
+  top: 0;
+  left: 0; 
+  right: 0;
+  z-index: 9999;
+`
+
+export const S = {
+  Header,
+}
