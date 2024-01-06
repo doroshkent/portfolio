5c1547d27ed299c77cfb470260229a24b332455c
doroshkent <108677278+doroshkent@users.noreply.github.com>
2024-01-06 14:37:55 +0100

    footer map refactor

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -5,17 +5,21 @@ import { FaLinkedinIn, FaTelegramPlane } from "react-icons/fa";
 import { Container } from "components/Container";
 import { S } from "./Footer_Styles";
 import { RiInstagramLine } from "react-icons/ri";
+import { v4 } from "uuid";
 
 const socialMediaData = [
   {
+    id: v4(),
     Icon: FaLinkedinIn,
     href: "https://www.linkedin.com/in/anastasiya-darashkevich-43901b270/"
   },
   {
+    id: v4(),
     Icon: RiInstagramLine,
     href: "https://www.instagram.com/doroshkent/"
   },
   {
+    id: v4(),
     Icon: FaTelegramPlane,
     href: "https://t.me/doroshkent"
   }
@@ -30,7 +34,7 @@ export const Footer: React.FC = () => {
             <S.Text>My social media links:</S.Text>
             <S.SocialList>
               { socialMediaData.map( sm => (
-                <S.SocialItem>
+                <S.SocialItem key={sm.id}>
                   <S.SocialLink href={ sm.href } target="_blank">
                     <sm.Icon style={ { color: "white", width: "30px", height: "30px" } } />
                   </S.SocialLink>
